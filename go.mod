06e043d011d3bb581d5b13ce1b4b026e1fc29a12
Anton Osenenko <anton.osenenko@gmail.com>
2023-07-28 12:55:42 +0200

    feat: Upgrade to go 1.16

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/a0s/terraform-provider-kubernetes-awaiter
 
-go 1.15
+go 1.16
 
 require (
 	github.com/go-resty/resty/v2 v2.6.0
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 )
 
-const VERSION = "0.0.1"
+const VERSION = "0.0.2"
 
 type Config struct {
 	Version bool `flag:"version" desc:"show version"`
